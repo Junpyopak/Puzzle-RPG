85cf9d0d8ced4cd45600391e2820c589e28a3c71
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2026-01-05 19:03:48 +0900

    플레이어 아이템 자석 기능

---
+++ Assets/Resources/Scripts/Ui_scripts/Item/ItemMagnet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMagnet : MonoBehaviour
+{
+    public float pullSpeed = 3;
+    public float pickupDistance= 3f;
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            float dist = Vector2.Distance(transform.position, player.position);
+            if (dist <= pickupDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.position, pullSpeed * Time.deltaTime);
+            }
+            
+        }
+    }
+}
