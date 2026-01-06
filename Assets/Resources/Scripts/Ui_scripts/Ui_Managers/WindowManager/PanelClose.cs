6ca5f5142e2705c74a4f6f87253cbf9254825fad
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2026-01-06 21:39:06 +0900

    게임 씬 불러오기

--- Assets/Resources/Scripts/Ui_scripts/Ui_Managers/WindowManager/PanelClose.cs
+++ Assets/Resources/Scripts/Ui_scripts/Ui_Managers/WindowManager/PanelClose.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PanelClose : MonoBehaviour
 {
     public GameObject panel;
+    public Player player;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,14 +25,46 @@ public class PanelClose : MonoBehaviour
     {
         panel.SetActive(!panel.activeSelf);
     }
+    //public void Exit()
+    //{
+    //    if (Scenemgr.Instance != null)
+    //    {
+    //        Scenemgr.Instance.ChangeScene(eSCENE.TITLE);
+    //    }
+    //    Debug.Log("게임을 끝내겠습니다");
+    //    panel.SetActive(!panel.activeSelf);
+    //}
     public void Exit()
     {
+        int slot = SaveContext.Instance.currentSlot;
+
+        // 슬롯이 선택된 상태라면 저장
+        if (slot >= 0)
+        {
+            SaveData data = new SaveData
+            {
+                //level = player.level,
+                playerExp = player.Exp,
+                playerHp = player.Hp,
+                playerPosition = player.transform.position,
+                saveTime = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm")
+            };
+
+            SaveManager.Save(slot, data);
+            Debug.Log($"슬롯 {slot} 저장 완료");
+        }
+        else
+        {
+            Debug.LogWarning("선택된 슬롯이 없어 저장하지 않았습니다");
+        }
+
+        // 타이틀로 이동
         if (Scenemgr.Instance != null)
         {
             Scenemgr.Instance.ChangeScene(eSCENE.TITLE);
         }
-        Debug.Log("게임을 끝내겠습니다");
-        panel.SetActive(!panel.activeSelf);
+
+        panel.SetActive(false);
     }
 }
     
