02f1a8fc6dad5e64aa1a66bbfe76c21b4cbe3785
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2026-02-16 17:52:36 +0900

    버블 보호막 위치 조정 작업중

--- Assets/Resources/Prefabs/CardResource/Ability/Bubble/BubbleShield.cs
+++ Assets/Resources/Prefabs/CardResource/Ability/Bubble/BubbleShield.cs
@@ -6,7 +6,7 @@ public class BubbleShield : MonoBehaviour
 {
     public Vector2Int offsetDir;
     private PlayerMove1 player;
-
+    public float baseYOffset = 0.25f;
     public void Init(PlayerMove1 player, Vector2Int dir)
     {
         this.player = player;
@@ -28,16 +28,18 @@ public class BubbleShield : MonoBehaviour
 
     void UpdatePosition()
     {
-        Vector2Int pos = player.gridPos + offsetDir;
-
         Vector2 cellSize = Grid15x15.Instance.cellWorldSize;
         int gridCount = Grid15x15.Instance.gridCount;
 
         float half = (gridCount - 1) / 2f;
 
-        float x = (pos.x - half) * cellSize.x;
-        float y = (pos.y - half) * cellSize.y;
+        Vector2Int pos = player.gridPos + offsetDir;
 
-        transform.position = new Vector3(x, y, 0f);
+        float x = (pos.x - half) * cellSize.x;
+
+        // 핵심: 기본 위치 + 위쪽 offset 추가
+        float y = (pos.y - half) * cellSize.y + cellSize.y * baseYOffset;
+
+        transform.position = new Vector3(x, y, player.transform.position.z);
     }
 }
