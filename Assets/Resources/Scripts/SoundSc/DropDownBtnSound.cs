596ad719f9e95b1ba1231dd7caa8406d9f73c738
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2026-02-23 22:57:35 +0900

    타이틀 화면 시작되자마자 버튼 효과음 나는거 오류 해결

--- Assets/Resources/Scripts/SoundSc/DropDownBtnSound.cs
+++ Assets/Resources/Scripts/SoundSc/DropDownBtnSound.cs
@@ -7,10 +7,16 @@ public class DropDownBtnSound : MonoBehaviour
     public AudioClip buttonDownSound;
 
     public string type; //sfx or bgm
-
+    private bool isInitialized = false;
     //드롭 다운 버튼의 사운드를 출력하는 함수
+    private void Start()
+    {
+        // Start 이후소리 나도록
+        isInitialized = true;
+    }
     public void BtnSoundPlay()
     {
+        if (!isInitialized) return;
         GameManager.Instance.SoundMgr.SoundPlay(type.ToString(), "버튼똑딱이는소리",buttonDownSound);
     }
 }
